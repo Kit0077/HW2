af55ba350eb692758df2cc04b48343261dc5da13
RpPrime <Nikitamatveev007@gmail.com>
2023-06-17 19:53:33 +0300

    Третие задание готово

---
+++ Ex3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7) Console.Write("Это выходной день");
+if (number != 6 && number != 7) Console.Write("Это не выходной день");
