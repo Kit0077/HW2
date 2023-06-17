1a7792bf9b05b7d5d4960ae631baf5d6793d4c29
RpPrime <Nikitamatveev007@gmail.com>
2023-06-17 13:53:14 +0300

    Первое задание Готово

---
+++ Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberToString = Convert.ToString(number);
+int lenght = numberToString.Length;
+
+if (lenght > 2 && lenght < 4)
+{
+    int a = number / 10;
+    int b = a % 10;
+    Console.Write($"Вторая цифра числа {number} равна {b}");
+}
+else Console.Write("Число не трехзначное");
